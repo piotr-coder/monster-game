f3023022c3a334c53bf4a703a7a9592233e68951
piotr-coder <blichu@wp.pl>
2022-01-08 19:30:09 +0100

    adding a "Game Over" Screen

--- App.js
+++ App.js
@@ -6,18 +6,41 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         }
     },
     computed: {
         monsterBarStyles() {
+            if(this.monsterHealth < 0){
+                return {width: '0%' };
+            }
             return {width: this.monsterHealth + '%'}; 
         },
         playerBarStyles() {
+            if(this.playerHealth < 0){
+                return {width: '0%' };
+            }
             return {width: this.playerHealth + '%'}; 
         },
         mayUseSpacialAttack(){
             return this.currentRound % 3 !== 0 || this.currentRound === 0;
+        }
+    },
+    watch: {
+        playerHealth(value) {
+            if(value<=0 && this.monsterHealth <=0){
+                this.winner = 'draw';
+            } else if (value<=0){
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value){
+            if(value <=0 && this.playerHealth <= 0){
+                this.winner = 'draw';
+            } else if (value <= 0){
+                this.winner = 'player';
+            }
         }
     },
     methods: {
@@ -46,6 +69,15 @@ const app = Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer();
+        },
+        startGame(){
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.winner = null;
+            this.currentRound = 0;
+        },
+        surrender(){
+            this.winner = 'monster';
         }
     },
 });
